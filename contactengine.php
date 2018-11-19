57e4263e1634f5c5121d49569577cda85cd7737e
Dominik Horvath <dom.horvath@gmail.com>
2018-11-19 20:13:04 +0100

    Added the whole website

---
+++ contactengine.php
@@ -0,0 +1,43 @@
+<?php
+
+$EmailFrom = "filub@filub.sk";
+$EmailTo = "filub@filub.sk";
+$Subject = "Kontaktný formulár";
+$Name = Trim(stripslashes($_POST['Name']));
+$Tel = Trim(stripslashes($_POST['Tel']));
+$Email = Trim(stripslashes($_POST['Email']));
+$Message = Trim(stripslashes($_POST['Message']));
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Tel: ";
+$Body .= $Tel;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=dakujeme.html\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
